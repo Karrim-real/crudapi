34d8e30d146ff70cb94f059529dd1034f2290bbf
Akinyemi Taiwo k <karrim4real@gmail.com>
2022-04-22 14:52:37 -0700

    DB data added

--- config/database.php
+++ config/database.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://kxpqmdxmsktbye:31aa231d9c1676a733acd6fc766ad92d5e389f3f5641f643e6d03c44df53094d@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d7t6cok8ehu20h');
 return [
 
     /*
@@ -16,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // 'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL['host'],
-            'port' =>$DATABASE_URL['port'] ,
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
